1d4443f48f7f59a5d402fbf4bec2647a78532e77
RohanImmanuel <i25.rohan@gmail.com>
2024-12-08 02:58:46 +0000

    toggle revert

--- js/transport.js
+++ js/transport.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $('#styleSelector').on('change', function () {
-        $('#theme').attr('href', $(this).val());
+    $("#themeToggleBtn").on("click", function () {
+        const themeLink = $("#themeStylesheet");
+        const currentTheme = themeLink.attr("href");
+
+        if (currentTheme === "css/anukrati.css") {
+            themeLink.attr("href", "css/rohan.css");
+            $(this).text("Switch to Anukrati's Styling");
+        } else {
+            themeLink.attr("href", "css/anukrati.css");
+            $(this).text("Switch to Rohan's Styling");
+        }
     });
 });
